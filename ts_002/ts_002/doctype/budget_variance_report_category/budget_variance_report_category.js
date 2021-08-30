df0c65ea84226e0603a8d901f2b28bc49404359c
marquelzikri <zmarquel@outlook.com>
2021-08-30 10:26:40 +0700

    First init

---
+++ ts_002/ts_002/doctype/budget_variance_report_category/budget_variance_report_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Kataba and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Budget Variance Report Category', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ ts_002/ts_002/doctype/budget_variance_report_category/test_budget_variance_report_category.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Kataba and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestBudgetVarianceReportCategory(unittest.TestCase):
+	pass
